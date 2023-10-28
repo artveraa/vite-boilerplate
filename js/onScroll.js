f5105fee0f51025892dca077eb2c47d4167c5aba
artvera <arthurvera@protonmail.com>
2023-10-28 17:46:40 +0200

    config gsap

--- js/onScroll.js
+++ js/onScroll.js
@@ -1,9 +1,31 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 class OnScroll {
     constructor() {
         this.init();
     }
 
     init() {
+        const animation = gsap.to('h1', {
+            duration: 1,
+            x: 100,
+            y: 100,
+            rotation: 360,
+            ease: "bounce.out"
+        })
+
+        ScrollTrigger.create({
+            trigger: ".main",
+            start: "top top",
+            end: "bottom top",
+            animation: animation,
+            markers: true
+        });
+
+
         console.log("OnScroll init")
     }
 }
